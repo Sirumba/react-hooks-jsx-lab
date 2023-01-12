860dd05a14b9618ec992b63859ac2c4666ccb2fe
Sirumba <simonsmaina45@gmail.com>
2023-01-12 19:40:51 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  const img = "I made this"
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>ADUhiBvckjAxbck</p>
+    <img src={image} alt={img} />
+  </div>;
 }
 
 export default About;
